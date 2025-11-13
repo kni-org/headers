4b975278afb4c97b3c693375731a58baf1156281
Niranjan Kumar K <kni.org.com@gmail.com>
2025-11-14 01:15:25 +0530

    Add/update kni.h

---
+++ headers/kni.h
@@ -0,0 +1,37 @@
+/*kni organisation for enhancing c starts on 13-11-2025 */
+/*organiser : Niranjan Kumar K */
+
+
+
+#ifndef kni //checking header file already exist or not
+#define kni //if not exist then to creating
+
+/* this is sample header file creating example */
+//here we create header file called kni
+// in header file . shall we add function for displaying 'Hi'
+
+
+
+#include<stdio.h>
+void f(){
+	printf("Hi");
+}
+
+
+
+#endif //header file ended 
+       
+
+
+       //after writing upload in platform
+       /* downloading and using procedure 
+	* download from platform and 
+	* save in your current directory(folder) or other wise
+	* move to c headers folder like includes...
+	* if donot no path platform itself provide you
+	* then simple use like #include "kni.h" (for header in current directory)
+	*# include <kni.h> (for header in c includes folder)
+	*
+	*Thanking You For Visiting !
+	*
+	*/
